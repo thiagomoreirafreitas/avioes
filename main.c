cdbacea12214ebe67ad44e515be6625da82e0a52
Thiago Moreira <thiagomoreirafreitas@gmail.com>
2019-11-20 01:22:04 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct aviao{
+    char nome[50];
+    int poltronas;
+    char origem;
+    char destino;
+};
+typedef struct aviao Aviao;
+int main()
+{
+    int i;
+    int quantidade = 0;
+    
+    printf("Digite quantos cadastros voce ira fazer: ");
+    scanf("%d", &quantidade);
+    Aviao avioes[quantidade]; 
+    
+    for(i=0; i<quantidade; i++)
+    {
+        printf("\nAviao %d\n",i);
+        printf("Digite nome: ");
+        scanf(" %[^\n]s",&avioes[i].nome);
+        printf("Digite o numero de poltronas: ");
+        scanf(" %d", &avioes[i].poltronas);
+        printf("Digite origem: ");
+        scanf(" %c", &avioes[i].origem);
+        printf("Digite destino: ");
+        scanf(" %c", &avioes[i].destino);
+    }
+    
+    for(i=0; i<quantidade; i++)
+    {
+        printf("\nAviao %d\n",i);
+        printf("Nome: %s\n", avioes[i].nome);
+        printf("Origem: %c\n", avioes[i].origem);
+        printf("Destino: %c\n", avioes[i].destino);
+    }
+
+    return 0;
+}
